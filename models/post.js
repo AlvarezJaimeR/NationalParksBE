2d1b3ba28b3d97642914d57067e0e07584594d87
Jaime Alvarez <jrralvarez@gmail.com>
2021-06-09 19:36:40 -0400

    finished the models for post, user, and routes for auth

---
+++ models/post.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const postSchema = new mongoose.Schema({
+  text: { type: String, required: true },
+  date: {type: Date, default:Date.now},
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+function validatePost(post) {
+  const schema = Joi.object({
+    text: Joi.string().required(),
+  });
+  return schema.validate(post);
+}
+
+exports.Post = Post;
+exports.validatePost = validatePost;
+exports.postSchema = postSchema;
---
+++ models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const { Post } = require("./post");
+
+const userSchema = new mongoose.Schema({
+    email: { type: String, require: true, minlength: 2, maxlength: 255 },
+    password: { type: String, require: true, minlength: 3, maxlength: 1024 },
+    firstName: { type: String, require: true, minlength: 2, maxlength: 50 },
+    lastName: { type: String, require: true, minlength: 2, maxlength: 50 },
+    posts: [Post.schema],
+});
+
+userSchema.methods.generateAuthToken = function () {
+    return jwt.sign(
+      {
+        _id: this._id,
+        firstName: this.firstName,
+        lastName: this.lastName,
+        posts: this.posts,
+      },
+      config.get("jwtSecret")
+    );
+  };
+  
+  const User = mongoose.model("User", userSchema);
+  
+  function validateUser(user) {
+    const schema = Joi.object({
+      email: Joi.string().min(2).max(255).required().email(),
+      password: Joi.string().min(3).max(1024).required(),
+      firstName: Joi.string().min(2).max(50).required(),
+      lastName: Joi.string().min(2).max(50).required(),
+    });
+    return schema.validate(user);
+  }
+  
+  exports.User = User;
+  exports.validateUser = validateUser;
+  exports.userSchema = userSchema;
