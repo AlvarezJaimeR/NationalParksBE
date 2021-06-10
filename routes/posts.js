118b059366b5665d77885661b31b5f15f4cc366c
Jaime Alvarez <jrralvarez@gmail.com>
2021-06-09 21:30:03 -0400

    able to edit a post

--- routes/posts.js
+++ routes/posts.js
@@ -38,8 +38,39 @@ router.post("/:id/post", async (req, res) => {
     }
 });
 
+//edit a post
+router.put("/:userId/edit/:postId", auth, async (req, res) => {
+    try {
+      const user = await User.findById(req.params.userId);
+      if (!user) return res.status(400).send(`The user id "${req.params.userId}" does not exist.`);
+      
+      console.log(user);
+
+      console.log(req.body);
+
+      const { error } = validatePost(req.body);
+      if (error) return res.status(400).send(error);
+
+      const keepPosts = user.posts.filter((post) => post._id != req.params.postId);
+      const filteredPosts = user.posts.filter((post) => post._id == req.params.postId);
+      
+      console.log('filterdPost-pre',filteredPosts);
+      filteredPosts[0].text = req.body.text
+      console.log('filterdPost-post',filteredPosts);
+
+      Array.prototype.push.apply(keepPosts, filteredPosts);
+      console.log(keepPosts);
+      user.posts = keepPosts;
+
+      await user.save();
+      return res.send(user);
+    } catch (ex) {
+      return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
 // delete a post
-router.put("/:userId/:postId", auth, async (req, res) => {
+router.put("/:userId/delete/:postId", auth, async (req, res) => {
     try {
       const user = await User.findByIdAndUpdate(req.params.userId);
       if (!user) return res.status(400).send(`The user id "${req.params.userId}" does not exist.`);
