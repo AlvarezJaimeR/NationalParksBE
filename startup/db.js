12307bdea1663d32c4d653f250d0bc4fb7e0157a
Jaime Alvarez <jrralvarez@gmail.com>
2021-08-09 15:03:45 -0400

    added mongoURI

--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 const config = require('config');
 
+var uri = process.env.MONGODB_URI || process.env.MONGOHQ_URL || process.env.MONGOLAB_URI;
+
 function connectDB() {
     mongoose.connect(
-        config.get('mongoURI'),
+        uri || config.get('mongoURI'),
         {useNewUrlParser: true, useUnifiedTopology: true})
         .then(() => console.log('Connected to MongoDB...'))
         .catch((err) => {
